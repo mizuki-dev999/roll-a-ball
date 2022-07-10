408a4cceaadee2f5c0a541418a7d31df45789552
mizuki_dev <mizuki.dev999@gmail.com>
2022-07-10 11:07:08 +0900

    add player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10; //プレイヤーの速度
+    private void FixedUpdate()
+    {
+        // 入力をxとzに代入
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        // 同一のGameObjectが持つRigidbodyコンポーネントを取得
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        // rigidbodyのx軸（横）とz軸（奥）に力を加える
+        rigidbody.AddForce(x * speed, 0, z * speed);
+    }
+}

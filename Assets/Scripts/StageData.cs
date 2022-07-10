1c73d23523871f54185262f007850868386d6f11
mizuki_dev <mizuki.dev999@gmail.com>
2022-07-10 18:07:55 +0900

    add StageData.cs CsvImporter.cs

---
+++ Assets/Scripts/StageData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create ScriptableObject/StageData")]
+public class StageData : ScriptableObject
+{
+    public int[][] stage;
+}
